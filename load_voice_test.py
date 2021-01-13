b756afee57886e9cb827250a367afcf8b1757f2c
YYlab <bittermax@gmail.com>
2021-01-13 14:00:42 +0300

    сэмпл с дефектом и без

--- load_voice_test.py
+++ load_voice_test.py
@@ -3,24 +3,39 @@
 import numpy as np
 #from pydub import AudioSegment
 import matplotlib.pyplot as plt
+from scipy import signal
+
 
 #audio = AudioSegment.from_file('D:\\YandexDisk\\Logoped\\science\\logorecon\\voice\\voice_test.m4a')
 #adata = np.array(audio.get_array_of_samples())
 
-import IPython.display as ipd
-import librosa
-
 import librosa.display
-filesample = 'D:\\YandexDisk\\Logoped\\science\\logorecon\\voice\\voice_test.m4a'
+filesample_defect = 'D:\\YandexDisk\\Logoped\\science\\logorecon\\voice\\трескучие январские морозы - дефект.m4a'
+filesample_norm = 'D:\\YandexDisk\\Logoped\\science\\logorecon\\voice\\трескучие январские морозы - норма.m4a'
+
 #ipd.Audio(filesample)
 plt.figure(figsize=(15,4))
-data, sample_rate1 = librosa.load(filesample, sr=22050, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
+data, sample_rate1 = librosa.load(filesample_defect, sr=44100, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
 librosa.display.waveplot(data,sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
+plt.title = filesample_defect
+#plt.show()
+print(sample_rate1)
+
+f, t, Sxx = signal.spectrogram(data, sample_rate1)
+
+plt.pcolormesh(t, f, Sxx, shading='gouraud')
+
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.show()
+
+plt.figure(figsize=(15,4))
+data, sample_rate1 = librosa.load(filesample_norm, sr=44100, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
+librosa.display.waveplot(data,sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
+plt.title = filesample_norm
 plt.show()
 print(sample_rate1)
 
-from scipy import signal
-from scipy.fft import fftshift
 f, t, Sxx = signal.spectrogram(data, sample_rate1)
 
 plt.pcolormesh(t, f, Sxx, shading='gouraud')
