434d52d05b7540e73704f5b6ff87e93f3d19b277
YYlab <bittermax@gmail.com>
2021-02-15 18:52:56 +0300

    mfcc коэфф и пара картинок с коэффициентами

--- load_voice_test.py
+++ load_voice_test.py
@@ -4,24 +4,46 @@ import numpy as np
 #from pydub import AudioSegment
 import matplotlib.pyplot as plt
 from scipy import signal
+import librosa.display
 
 
 #audio = AudioSegment.from_file('D:\\YandexDisk\\Logoped\\science\\logorecon\\voice\\voice_test.m4a')
 #adata = np.array(audio.get_array_of_samples())
 
-import librosa.display
-filesample_defect = 'D:\\YandexDisk\\Logoped\\science\\logorecon\\voice\\трескучие январские морозы - дефект.m4a'
-filesample_norm = 'D:\\YandexDisk\\Logoped\\science\\logorecon\\voice\\трескучие январские морозы - норма.m4a'
+
+filesample_defect = 'voice\\трескучие январские морозы - дефект.m4a'
+filesample_norm = 'voice\\трескучие январские морозы - норма.m4a'
 
 #ipd.Audio(filesample)
 plt.figure(figsize=(15,4))
-data, sample_rate1 = librosa.load(filesample_defect, sr=44100, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
-librosa.display.waveplot(data,sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
-plt.title = filesample_defect
+signal, sample_rate1 = librosa.load(filesample_norm, sr=44100, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
+signal_d, sample_rate1_d = librosa.load(filesample_defect, sr=44100, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
+
+librosa.display.waveplot(signal, sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
+librosa.display.waveplot(signal_d, sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
+
 #plt.show()
 print(sample_rate1)
 
-f, t, Sxx = signal.spectrogram(data, sample_rate1)
+#mfcc part
+mfcc = librosa.feature.mfcc(signal, n_mfcc=13, sr=sample_rate1)
+
+mfcc_d = librosa.feature.mfcc(signal_d, n_mfcc=13, sr=sample_rate1_d)
+
+plt.figure(figsize=(25, 10))
+librosa.display.specshow(mfcc, x_axis="time", sr=sample_rate1)
+plt.colorbar(format="%+2f")
+plt.show()
+
+plt.figure(figsize=(25, 10))
+librosa.display.specshow(mfcc_d, x_axis="time", sr=sample_rate1_d)
+plt.colorbar(format="%+2f")
+plt.show()
+
+exit()
+
+
+f, t, Sxx = signal.spectrogram(signal, sample_rate1)
 
 plt.pcolormesh(t, f, Sxx, shading='gouraud')
 
@@ -30,13 +52,13 @@ plt.xlabel('Time [sec]')
 plt.show()
 
 plt.figure(figsize=(15,4))
-data, sample_rate1 = librosa.load(filesample_norm, sr=44100, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
-librosa.display.waveplot(data,sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
+signal, sample_rate1 = librosa.load(filesample_norm, sr=44100, mono=True, offset=0.0, duration=50, res_type='kaiser_best')
+librosa.display.waveplot(signal, sr=sample_rate1, max_points=50000.0, x_axis='time', offset=0.0, max_sr=1000)
 plt.title = filesample_norm
 plt.show()
 print(sample_rate1)
 
-f, t, Sxx = signal.spectrogram(data, sample_rate1)
+f, t, Sxx = signal.spectrogram(signal, sample_rate1)
 
 plt.pcolormesh(t, f, Sxx, shading='gouraud')
 plt.ylabel('Frequency [Hz]')
